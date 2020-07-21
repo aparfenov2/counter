5a163ace74155b25fc5e7ff58f30b0700f39e41f
Aleksey Parfyonov <kantengri3@gmail.com>
2020-07-22 00:32:45 +0500

    inst fix

--- jenkins_entry.sh
+++ jenkins_entry.sh
@@ -1,4 +1,6 @@
 set -ex
+. ./jenkins_env.sh
+env
 
 [ -d '.env3' ] || {
     python3.7 -m venv .env3
@@ -7,11 +9,16 @@ set -ex
 cd yolov5
 
 [ -d '../data/digits' ] || {
-    echo "train dataset not found"
-    exit 1
+    echo "train dataset not found - trying download"
+    mkdir -p ../data || true
+    cd ../data
+    scp pi@kantengri.ddns.net:digits.tgz .
+    tar xvf digits.tgz
+    ls -l
+    cd ../yolov5 
 }
 ln -s ../data/digits data/digits || true
-
+ls -l data/digits
 python -m pip install -r requirements.txt
 # bash train.sh
 python train.py --img 640 --batch 16 --epochs 5 --data ./data/digits.yaml --cfg ./models/digits.yaml --weights ''
