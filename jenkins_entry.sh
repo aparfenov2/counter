d36451555c120fe2d36a2f2cf2d25fa39b68f71e
Aleksey Parfyonov <kantengri3@gmail.com>
2020-07-22 01:49:18 +0500

    conda

--- jenkins_entry.sh
+++ jenkins_entry.sh
@@ -2,10 +2,17 @@ set -ex
 . ./jenkins_env.sh
 env
 
-[ -d '.env3' ] || {
-    /opt/conda/bin/python3.7 -m venv .env3 --system-site-packages
-}
-. .env3/bin/activate
+conda update -yn base -c defaults conda
+conda install -yc anaconda numpy opencv matplotlib tqdm pillow ipython
+conda install -yc conda-forge scikit-image pycocotools tensorboard
+conda install -yc spyder-ide spyder-line-profiler
+conda install -yc pytorch pytorch torchvision
+conda install -yc conda-forge protobuf numpy && pip install onnx==1.6.0  # https://github.com/onnx/onnx#linux-and-macos
+
+# [ -d '.env3' ] || {
+#     /opt/conda/bin/python3.7 -m venv .env3 --system-site-packages
+# }
+# . .env3/bin/activate
 cd yolov5
 
 [ -d '/root/data/digits' ] || {
@@ -22,9 +29,9 @@ ls -l data/digits
 
 python -m pip install -r requirements.txt
 
-apt update
-apt install -y libglib2.0-0
-apt install -y libsm6 libxext6
-apt install -y libxrender-dev
-# bash train.sh
+# apt update
+# apt install -y libglib2.0-0
+# apt install -y libsm6 libxext6
+# apt install -y libxrender-dev
+
 python train.py --img 640 --batch 16 --epochs 5 --data ./data/digits.yaml --cfg ./models/digits.yaml --weights ''
