c0dedf08a55bdf707e88ce799dbd665fc98d7dd5
Aleksey Parfyonov <kantengri3@gmail.com>
2020-07-22 02:04:54 +0500

    back to pip

--- jenkins_entry.sh
+++ jenkins_entry.sh
@@ -1,18 +1,18 @@
 set -ex
 . ./jenkins_env.sh
 env
-PATH=$PATH:/opt/conda/bin:/usr/local/nvidia/bin:/usr/local/cuda/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
-conda update -yn base -c defaults conda
-conda install -yc anaconda numpy opencv matplotlib tqdm pillow ipython
-conda install -yc conda-forge scikit-image pycocotools tensorboard
-conda install -yc spyder-ide spyder-line-profiler
-conda install -yc pytorch pytorch torchvision
-conda install -yc conda-forge protobuf numpy && pip install onnx==1.6.0  # https://github.com/onnx/onnx#linux-and-macos
+# PATH=$PATH:/opt/conda/bin:/usr/local/nvidia/bin:/usr/local/cuda/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
+# conda update -yn base -c defaults conda
+# conda install -yc anaconda numpy opencv matplotlib tqdm pillow ipython
+# conda install -yc conda-forge scikit-image pycocotools tensorboard
+# conda install -yc spyder-ide spyder-line-profiler
+# conda install -yc pytorch pytorch torchvision
+# conda install -yc conda-forge protobuf numpy && pip install onnx==1.6.0  # https://github.com/onnx/onnx#linux-and-macos
 
-# [ -d '.env3' ] || {
-#     /opt/conda/bin/python3.7 -m venv .env3 --system-site-packages
-# }
-# . .env3/bin/activate
+[ -d '.env3' ] || {
+    /opt/conda/bin/python3.7 -m venv .env3 --system-site-packages
+}
+. .env3/bin/activate
 cd yolov5
 
 [ -d '/root/data/digits' ] || {
@@ -27,7 +27,7 @@ cd yolov5
 ln -s /root/data/digits data/digits || true
 ls -l data/digits
 
-# python -m pip install -r requirements.txt
+python -m pip install -r requirements.txt
 
 apt update
 apt install -y libglib2.0-0
